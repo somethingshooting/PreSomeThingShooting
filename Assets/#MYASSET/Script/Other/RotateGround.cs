dfcf5e6325630b709fd4c25458b94be42c4bdee7
そな <lite01250@gmail.com>
2019-12-06 17:42:46 +0900

    Add HitArea and Ground

---
+++ Assets/#MYASSET/Script/Other/RotateGround.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGround : MonoBehaviour
+{
+    [SerializeField] private float _RotateSpeed = 1;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * _RotateSpeed * Time.deltaTime);
+    }
+}
